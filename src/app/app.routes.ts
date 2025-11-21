2261ecb80f79ac0629ccda143275a07d629b7c2f
pablo llanos <romaypablo19@gmail.com>
2025-11-20 21:34:32 -0400

    Arreglar rutas y base href para GitHub Pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,23 @@
+
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { Routes } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { GALERIA } from './pages/galeria/galeria';
 import { Contactos } from './pages/contactos/contactos';
 import { Home } from './pages/home/home';
 import { Resumen } from './pages/resumen/resumen';
 
 export const routes: Routes = [
-    {path: '', component: Home},
-    {path: 'galeria',component: GALERIA},
-    {path: 'contactos',component: Contactos},
-    {path: 'resumen', component: Resumen}
+  { path: '', component: Home },
+  { path: 'galeria', component: GALERIA },
+  { path: 'contactos', component: Contactos },
+  { path: 'resumen', component: Resumen }
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }]
+})
+export class AppRoutingModule {}
